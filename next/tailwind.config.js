e0e37f29adce5daefa60e68cbe19be971ac5d324
shawn-flunge <echun1234@gmail.com>
2023-09-21 20:55:26 +0900

    폰트관련 파일, 설정 제거

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
-        sans: ['Pretendard', ]
+        // sans: ['Pretendard', ]
         // sans: ['RobotoFlex']
       }
     },
