8e8b98e14e9067e69f5ae4207dfd2b057ff81bcd
shawn-flunge <echun1234@gmail.com>
2023-10-04 21:22:34 +0900

    블로그 게시물 중 최근 3개 끌고와서 보여줌

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,8 +1,28 @@
+import { BlogCard } from "@/components/card";
+import { getFrontmatters } from "@/components/mdx"
+import FrontMatter from "@/interfaces/FrontMatter"
 
-export default function Home2(){
+export default function Home2({ latest }: { latest: FrontMatter[] }){
+
     return <div className="container px-2 pt-32 mx-auto">
-        <p className="h-full">
-            Comming soon.
-        </p>
+        <div className="text-3xl font-medium">
+            Latest Posts
+        </div>
+        <ul className="container mx-auto">
+            { latest.map((matter) => BlogCard(matter)) }
+        </ul>
     </div>
+}
+
+export async function getStaticProps() {
+    
+    const matters =  getFrontmatters();
+    matters.sort((a, b) => a.id-b.id);
+    // todo : 블로그 글 많아지면 length -3 으로 고정
+    const start = matters.length > 3 ? matters.length-3 : 0;
+    const latest = matters.slice(start);
+
+    return {
+        props: { latest },
+    }
 }
