5e740b5312fbba3a5ada50eb10dacdd5815b835f
shawn-flunge <echun1234@gmail.com>
2024-02-19 19:26:41 +0900

    다른 버튼 없애고 이력서 다운 버튼으로 대체

--- next/src/components/NavBar.tsx
+++ next/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -8,7 +9,6 @@ type NavItemType = {route: string, name: string};
 
 function NavBar(){
     const pages : NavItemType[] = [
-        {route: '/me', name: 'Me'},
         {route: '/blog', name: 'Blog'},
     ]
 
@@ -17,20 +17,13 @@ function NavBar(){
 
             <Link key='/' href='/'>
                 <div className="font-mono text-3xl sm:px-4">
-                    Shawn:~/
+                    Shawn
                 </div>
             </Link>
             
-            <div className="sm:px-4">
-                {
-                    pages?.map((page: NavItemType) => (
-                        <Link href={page.route} key={page.route} className="p-1 font-mono font-medium text-gray-900 dark:text-gray-100">
-                            {page.name}
-                        </Link>
-                    ))
-                }
-            </div>
-            
+            <a href="/resume.pdf" download="Flutter개발자 이시헌" className="p-2 ">
+                RESUME
+            </a>
         </nav>
 
     </>
